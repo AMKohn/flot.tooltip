c984fc4d37f260d2cc143dd88ed454d3f322b7d0
AMKohn <h.e.kohn824@gmail.com>
2013-07-31 10:39:41 -0400

    Changed tooltip position to detect edges and avoid them

--- js/jquery.flot.tooltip.js
+++ js/jquery.flot.tooltip.js
@@ -2,84 +2,56 @@
  * jquery.flot.tooltip
  * 
  * description: easy-to-use tooltips for Flot charts
- * version: 0.6.1
- * author: Krzysztof Urbas @krzysu [myviews.pl]
- * website: https://github.com/krzysu/flot.tooltip
+ * version: 0.7
+ * modified by Avi kohn
  * 
- * build on 2013-07-10
- * released under MIT License, 2012
+ * built on 2013-07-31
 */ 
 (function ($) {
-
-    // plugin options, default values
     var defaultOptions = {
         tooltip: false,
         tooltipOpts: {
             content: "%s | X: %x | Y: %y",
-            // allowed templates are:
-            // %s -> series label,
-            // %x -> X value,
-            // %y -> Y value,
-            // %x.2 -> precision of X value,
-            // %p -> percent
-            xDateFormat: null,
-            yDateFormat: null,
             shifts: {
-                x: 10,
-                y: 20
+                x: 0,
+                y: 0
             },
-            defaultTheme: true,
-
-            // callbacks
             onHover: function(flotItem, $tooltipEl) {}
         }
     };
 
-    // object
     var FlotTooltip = function(plot) {
-
-        // variables
         this.tipPosition = {x: 0, y: 0};
 
         this.init(plot);
     };
 
-    // main plugin function
     FlotTooltip.prototype.init = function(plot) {
 
         var that = this;
 
         plot.hooks.bindEvents.push(function (plot, eventHolder) {
-
-            // get plot options
             that.plotOptions = plot.getOptions();
 
-            // if not enabled return
             if (that.plotOptions.tooltip === false || typeof that.plotOptions.tooltip === 'undefined') return;
 
-            // shortcut to access tooltip options
             that.tooltipOptions = that.plotOptions.tooltipOpts;
 
-            // create tooltip DOM element
-            var $tip = that.getDomElement();
+            var $tip = that.getDomElement(plot.getPlaceholder());
 
-            // bind event
-            $( plot.getPlaceholder() ).bind("plothover", function (event, pos, item) {
+            plot.getPlaceholder().bind("plothover", function (event, pos, item) {
                 if (item) {
                     var tipText;
 
-                    // convert tooltip content template to real tipText
                     tipText = that.stringFormat(that.tooltipOptions.content, item);
 
-                    $tip.html( tipText );
-                    that.updateTooltipPosition({ x: item.pageX, y: item.pageX });
+                    $tip.html(tipText);
+                    that.updateTooltipPosition({ x: item.pageX - plot.getPlaceholder().offset().left, y: item.pageY - plot.getPlaceholder().offset().top }, plot.getPlaceholder());
                     $tip.css({
                             left: that.tipPosition.x + that.tooltipOptions.shifts.x,
                             top: that.tipPosition.y + that.tooltipOptions.shifts.y
-                        })
-                        .show();
+                        }).show();
 
-                    // run callback
                     if(typeof that.tooltipOptions.onHover === 'function') {
                         that.tooltipOptions.onHover(item, $tip);
                     }
@@ -91,11 +63,7 @@
         });
     };
 
-    /**
-     * get or create tooltip DOM element
-     * @return jQuery object
-     */
-    FlotTooltip.prototype.getDomElement = function() {
+    FlotTooltip.prototype.getDomElement = function(elm) {
         var $tip;
 
         if( $('#flotTip').length > 0 ){
@@ -103,65 +71,43 @@
         }
         else {
             $tip = $('<div />').attr('id', 'flotTip');
-            $tip.appendTo('body').hide().css({position: 'absolute'});
+            $tip.appendTo(elm).hide().css({position: 'absolute'});
         }
 
         return $tip;
     };
 
-    // as the name says
-    FlotTooltip.prototype.updateTooltipPosition = function(pos) {
-        var tipWidth = $("#flotTip").outerWidth() / 2;
-        var tipHeight = $("#flotTip").outerHeight() / 2;
-        
-        this.tipPosition.x = pos.x - tipWidth + this.tooltipOptions.shifts.x;
-        this.tipPosition.y = pos.y - tipHeight + this.tooltipOptions.shifts.y;
+    FlotTooltip.prototype.updateTooltipPosition = function(pos, elm) {
+        var tipWidth = $("#flotTip").outerWidth() / 2,
+            tipHeight = $("#flotTip").outerHeight(),
+        	elmWidth = elm.outerWidth(),
+        	elmHeight = elm.outerHeight();
+
+	    this.tipPosition.x = pos.x - tipWidth + this.tooltipOptions.shifts.x;
+	    this.tipPosition.y = pos.y - tipHeight - 15 + this.tooltipOptions.shifts.y;
+
+        while (this.tipPosition.x + tipWidth * 2 > elm.outerWidth()) this.tipPosition.x--;
+        while (this.tipPosition.y + tipHeight > elm.outerHeight()) this.tipPosition.y--;
+
+        if (this.tipPosition.x < 0) this.tipPosition.x = 0;
+        if (this.tipPosition.y < 0) this.tipPosition.y = pos.y + 15 + this.tooltipOptions.shifts.y;
     };
 
-    /**
-     * core function, create tooltip content
-     * @param  {string} content - template with tooltip content
-     * @param  {object} item - Flot item
-     * @return {string} real tooltip content for current item
-     */
     FlotTooltip.prototype.stringFormat = function(content, item) {
-        if( typeof(content) === 'function' ) {
-            content = content(item.series.label, item.series.data[item.dataIndex][0], item.series.data[item.dataIndex][1]);
-        }
-        
+        content = content(item.series.label, item.series.data[item.dataIndex][0], item.series.data[item.dataIndex][1]);
+
         return content;
     };
 
-    // helpers just for readability
-    FlotTooltip.prototype.isTimeMode = function(axisName, item) {
-        return (typeof item.series[axisName].options.mode !== 'undefined' && item.series[axisName].options.mode === 'time');
-    };
-
-    FlotTooltip.prototype.isXDateFormat = function(item) {
-        return (typeof this.tooltipOptions.xDateFormat !== 'undefined' && this.tooltipOptions.xDateFormat !== null);
-    };
-
-    FlotTooltip.prototype.isYDateFormat = function(item) {
-        return (typeof this.tooltipOptions.yDateFormat !== 'undefined' && this.tooltipOptions.yDateFormat !== null);
-    };
-
-    //
-    FlotTooltip.prototype.timestampToDate = function(tmst, dateFormat) {
-        var theDate = new Date(tmst);
-        return $.plot.formatDate(theDate, dateFormat);
-    };
-
-    //
     var init = function(plot) {
       new FlotTooltip(plot);
     };
 
-    // define Flot plugin
     $.plot.plugins.push({
         init: init,
         options: defaultOptions,
         name: 'tooltip',
-        version: '0.6.1'
+        version: '0.7'
     });
 
 })(jQuery);
