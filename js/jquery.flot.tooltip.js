77f6e7eaa7a6f149d0b66c280d991478ace984d5
AMKohn <h.e.kohn824@gmail.com>
2013-07-31 09:40:40 -0400

    Changed tooltip position to be point based

--- js/jquery.flot.tooltip.js
+++ js/jquery.flot.tooltip.js
@@ -72,7 +72,7 @@
                     tipText = that.stringFormat(that.tooltipOptions.content, item);
 
                     $tip.html( tipText );
-                    that.updateTooltipPosition({ x: pos.pageX, y: pos.pageY });
+                    that.updateTooltipPosition({ x: item.pageX, y: item.pageX });
                     $tip.css({
                             left: that.tipPosition.x + that.tooltipOptions.shifts.x,
                             top: that.tipPosition.y + that.tooltipOptions.shifts.y
@@ -87,13 +87,6 @@
                 else {
                     $tip.hide().html('');
                 }
-            });
-
-            eventHolder.mousemove( function(e) {
-                var pos = {};
-                pos.x = e.pageX;
-                pos.y = e.pageY;
-                that.updateTooltipPosition(pos);
             });
         });
     };
@@ -111,19 +104,6 @@
         else {
             $tip = $('<div />').attr('id', 'flotTip');
             $tip.appendTo('body').hide().css({position: 'absolute'});
-
-            if(this.tooltipOptions.defaultTheme) {
-                $tip.css({
-                    'background': '#fff',
-                    'z-index': '100',
-                    'padding': '0.4em 0.6em',
-                    'border-radius': '0.5em',
-                    'font-size': '0.8em',
-                    'border': '1px solid #111',
-                    'display': 'inline-block',
-                    'white-space': 'nowrap'
-                });
-            }
         }
 
         return $tip;
@@ -150,52 +130,10 @@
      * @return {string} real tooltip content for current item
      */
     FlotTooltip.prototype.stringFormat = function(content, item) {
-
-        var percentPattern = /%p\.{0,1}(\d{0,})/;
-        var seriesPattern = /%s/;
-        var xPattern = /%x\.{0,1}(?:\d{0,})/;
-        var yPattern = /%y\.{0,1}(?:\d{0,})/;
-
-        // if it is a function callback get the content string
         if( typeof(content) === 'function' ) {
             content = content(item.series.label, item.series.data[item.dataIndex][0], item.series.data[item.dataIndex][1]);
         }
-
-        // percent match for pie charts
-        if( typeof (item.series.percent) !== 'undefined' ) {
-            content = this.adjustValPrecision(percentPattern, content, item.series.percent);
-        }
-
-        // series match
-        if( typeof(item.series.label) !== 'undefined' ) {
-            content = content.replace(seriesPattern, item.series.label);
-        }
-
-        // time mode axes with custom dateFormat
-        if(this.isTimeMode('xaxis', item) && this.isXDateFormat(item)) {
-            content = content.replace(xPattern, this.timestampToDate(item.series.data[item.dataIndex][0], this.tooltipOptions.xDateFormat));
-        }
-
-        if(this.isTimeMode('yaxis', item) && this.isYDateFormat(item)) {
-            content = content.replace(yPattern, this.timestampToDate(item.series.data[item.dataIndex][1], this.tooltipOptions.yDateFormat));
-        }
-
-        // set precision if defined
-        if( typeof item.series.data[item.dataIndex][0] === 'number' ) {
-            content = this.adjustValPrecision(xPattern, content, item.series.data[item.dataIndex][0]);
-        }
-        if( typeof item.series.data[item.dataIndex][1] === 'number' ) {
-            content = this.adjustValPrecision(yPattern, content, item.series.data[item.dataIndex][1]);
-        }
-
-        // if no value customization, use tickFormatter by default
-        if(typeof item.series.xaxis.tickFormatter !== 'undefined') {
-            content = content.replace(xPattern, item.series.xaxis.tickFormatter(item.series.data[item.dataIndex][0], item.series.xaxis));
-        }
-        if(typeof item.series.yaxis.tickFormatter !== 'undefined') {
-            content = content.replace(yPattern, item.series.yaxis.tickFormatter(item.series.data[item.dataIndex][1], item.series.yaxis));
-        }
-
+        
         return content;
     };
 
@@ -216,23 +154,6 @@
     FlotTooltip.prototype.timestampToDate = function(tmst, dateFormat) {
         var theDate = new Date(tmst);
         return $.plot.formatDate(theDate, dateFormat);
-    };
-
-    //
-    FlotTooltip.prototype.adjustValPrecision = function(pattern, content, value) {
-
-        var precision;
-        var matchResult = content.match(pattern);
-        if( matchResult !== null ) {
-            if(RegExp.$1 !== '') {
-                precision = RegExp.$1;
-                value = value.toFixed(precision);
-
-                // only replace content if precision exists, in other case use thickformater
-                content = content.replace(pattern, value);
-            }
-        }
-        return content;
     };
 
     //
